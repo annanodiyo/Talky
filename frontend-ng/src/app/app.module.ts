d77261998ef97f158a6b44c2a41da90df0b447dd
annanodiyo <annanodiyo@gmail.com>
2023-12-06 10:13:13 +0300

    talky application frontend with angular

---
+++ frontend-ng/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './register/register.component';
+import { AsideComponent } from './aside/aside.component';
+import { HomeComponent } from './home/home.component';
+import { CreatepostComponent } from './createpost/createpost.component';
+import { EditprofileComponent } from './editprofile/editprofile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    FooterComponent,
+    RegisterComponent,
+    AsideComponent,
+    HomeComponent,
+    CreatepostComponent,
+    EditprofileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
